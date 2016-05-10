c70f86bc94da7f667ffe2eb09e4c233a3c00caf9
zuopeng <2838137011@qq.com>
2016-05-10 14:53:25 +0800

    patch

--- patch/ViewController.lua
+++ patch/ViewController.lua
@@ -1,23 +1,39 @@
 waxClass{"ViewController",UIViewController}
-IBOutlet "label"
+-- IBOutlet "label"
 -- function viewDidLoad(self)
 
 -- print(viewDidLoad)
 
 -- end
 
-function viewDidLoad(self)
+-- function viewDidLoad(self)
 
-print(112)
-self.super:viewDidLoad(self)
-	self.label:setColor(UIColor:greenColor())
-	self.label:setText("我还有俩只视野,从来也不插0_0")
-print(113)
+-- print(112)
+-- self.super:viewDidLoad(self)
+-- 	self.label:setColor(UIColor:greenColor())
+-- 	self.label:setText("我还有俩只视野,从来也不插0_0")
+-- print(113)
 
-end
+-- end
 
 -- function viewDidLoad(self)
 -- 	self.super:viewDidLoad(self)
 -- 	self.label:setColor(UIColor:greenColor())
 -- 	self.label:setText("我还有俩只视野,从来也不插0_0")
 -- end
+
+function viewDidLoad(self)
+
+print(112)
+self.super:viewDidLoad(self)
+	local label = UILabel:initWithFrame(CGRect(0, 100, 320, 35))  
+    label:setFont(UIFont:boldSystemFontOfSize(30))  
+    label:setColor(UIColor:whiteColor())  
+    label:setBackgroundColor(UIColor:colorWithRed_green_blue_alpha(0.545, 0.3, 1, 1))  
+    label:setText("前方高能：")  
+    label:setTextAlignment(UITextAlignmentCenter)  
+    self.view:addSubview(label)  
+print(113)
+
+end
+
