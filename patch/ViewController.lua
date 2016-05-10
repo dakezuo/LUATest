73ab5e9934f1be35dd557929f900d0e95e13ec19
zuopeng <2838137011@qq.com>
2016-05-10 16:20:39 +0800

    patch

--- patch/ViewController.lua
+++ patch/ViewController.lua
@@ -10,17 +10,11 @@ function viewDidLoad(self)
 
 print(112)
 self.super:viewDidLoad(self)
-	self:label():setColor(UIColor:greenColor())
+	-- self:label():setColor(UIColor:greenColor())
 	self:label():setText("我还有俩只视野,从来也不插0_0")
 print(113)
 
 end
-
--- function viewDidLoad(self)
--- 	self.super:viewDidLoad(self)
--- 	self.label:setColor(UIColor:greenColor())
--- 	self.label:setText("我还有俩只视野,从来也不插0_0")
--- end
 
 -- function viewDidLoad(self)
 
